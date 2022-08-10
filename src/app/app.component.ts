090f16c0186a99aeb2f80b2afa3a1dcf834b16c2
JMaycol-Valencia <maycolvalencia50@gmail.com>
2022-08-10 17:14:31 -0400

    componente-store-inicioDeProyecto

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'componente-store';
+  ayudaCodigo = 'https://stackoverflow.com/questions/64573177/unable-to-resolve-dependency-tree-error-when-installing-npm-packages';
+}
