44b3cf773623f58326821eabd08912295d1e83e3
JMaycol-Valencia <maycolvalencia50@gmail.com>
2022-08-11 10:43:24 -0400

    componente-store-conociendoComponentes

--- src/app/componentes/img/img.component.ts
+++ src/app/componentes/img/img.component.ts
@@ -1,5 +1,10 @@
+/*Archivo que manejara la logica de nuestro componente*/
 import { Component, OnInit } from '@angular/core';
 
+/*EN ANGULAR LO SIG LO CONOCEMOS COMO DECORADOR
+	SELECTOR -> NOMBRE CON EL QUE PODREMOS USAR EL COMPONENTE
+	TEMPLATEURL -> ARCHIVO HTML QUE FUNCIONA COMO VISTA
+	STYLEURLS -> HOJA DE ESTILOS PERSONALIZADA PARA EL COMPONENTE*/
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
