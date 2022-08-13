980384002b59d98252ef649d021d26851d80fab7
JMaycol-Valencia <maycolvalencia50@gmail.com>
2022-08-12 23:13:09 -0400

    componete-store-terminaciones

--- src/app/componentes/img/img.component.ts
+++ src/app/componentes/img/img.component.ts
@@ -1,5 +1,5 @@
 /*Archivo que manejara la logica de nuestro componente*/
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy } from '@angular/core';
 
 /*EN ANGULAR LO SIG LO CONOCEMOS COMO DECORADOR
 	SELECTOR -> NOMBRE CON EL QUE PODREMOS USAR EL COMPONENTE
@@ -10,16 +10,45 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
 
 
   @Input() img: string = '';
   @Output() loaded = new EventEmitter<string>();
   imageDefault = "./assets/imagenes/default.png"
 
-  constructor() { }
+  constructor() {
+    //lo primero en renderizarse
+    //solo corre una vez
+    //no correr codigo de forma asyncrona
+    console.log('Constructor', 'imgValue =>', this.img);
+  }
+
+  //CORRE AL INICIAR EL RENDER
+  //MONITOREA LOS CAMBIOS EN LOS INPUTS
+  //CORRE TODAS LAS VECES QUE CORRE NUESTROS INPUTS
+  ngOnChanges() {
+    console.log('ngOnChanges', 'imgValue =>', this.img);
+  }
 
   ngOnInit(): void {
+    //CORRE ANTES DEL RENDER
+    //PODEMOS CORRER COSAS ASYNCRONAS
+    //LLAMADAS FETCH , LLAMADAS DE API'S
+    //CORRE UNA SOLA VEZ
+    console.log('ngOnInit', 'imgValue =>', this.img);
+  }
+
+  ngAfterViewInit() {
+    //CORRE DESPUES DE QUE TODO SE RENDERIZE
+    //MANEJAMOS LOS COMPONENTES HIJOS
+    console.log('ngAfterViewInit');
+
+  }
+
+  ngOnDestroy() {
+      //elimina el componente
+      console.log('ngOnDestroy');
   }
 
   imgError(){
